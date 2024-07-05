bd808cc7ca0caf6c6905858fc176b4b384725c44
armor-gym <armorgymgombong@gmail.com>
2024-07-05 18:02:47 +0700

    fix tampilan owner master

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -113,7 +113,7 @@ class LoginController extends Controller
 
             } elseif(Auth::user()->role == 'OWNER'){
                 Alert::success('Success', 'Successfully Login Owner');
-                return redirect()->route('owner.home');
+                return redirect()->route('report.index-monthly');
 
             } elseif(Auth::user()->role === 'GUEST'){
                 Alert::success('Success', 'Successfully Login Guest');
